5a8babdc8c476b31b96e7cba846ec64857fecf98
Matias Neira <matiasneirab@gmail.com>
2022-04-05 19:31:07 -0300

    titulo card

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -8,7 +8,7 @@ const Register = () => {
                         <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
                             <div class="card border-0 shadow rounded-3 my-5">
                                 <div class="card-body p-4 p-sm-5">
-                                    <h5 class="card-title text-center mb-5 fw-light fs-5"><h3><strong>CREA TU CUENTA</strong></h3></h5>
+                                    <h4 class="card-title text-center mb-5 fw-light fs-5"><strong>CREA TU CUENTA</strong></h4>
                                     <form>
                                     <div class="form-floating mb-3">
                                             <input type="text" class="form-control" id="floatingInput" placeholder="Nombre"/>
